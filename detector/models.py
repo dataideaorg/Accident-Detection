8d7b1a53f3698b50cfdf276bb1cd274efb8c35c2
jumashafara <jumashafara0@gmail.com>
2023-05-05 14:55:31 +0000

    added prediction data model

--- detector/models.py
+++ detector/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Prediction(models.Model):
+    prediction = models.CharField(max_length=255)
+    confidence = models.FloatField()
+    date_time = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.prediction} with {self.confidence}% confidence at {self.date_time} in {self.location}"
